43945a50e324fc3553da0bd11b1939eff1751301
Manish Biswal <manishsom20@gmail.com>
2021-01-07 01:49:18 +0530

    Update: fetching done

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 var btnTranslate = document.querySelector("#btn-Translate");
 var txtInput = document.querySelector("#txt-Input");
-btnTranslate.addEventListener("click", clickHandler());
+var outputDiv = document.querySelector("#output-div");
+
+var severurl = "https://lessonfourapi.som021100.repl.co/translate/yoda.json";
+function constUrl(text) {
+  return severurl + "?text=" + text;
+}
+function errorhandler (error){
+  console.log("Error occurred");
+  alert("Server is down!! Plese try latter");
+}
 function clickHandler() {
-  console.log("Clicked!!");
+  var inputText = txtInput.value
+  fetch(constUrl(inputText))
+    .then(Response => Response.json())
+    .then(json => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText.value;
+    })
+    .catch(errorhandler)
 };
+btnTranslate.addEventListener("click", clickHandler);
