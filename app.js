f319ed360fe805445ce93334d3941d87f68e81e6
Manish Biswal <manishsom20@gmail.com>
2021-01-06 12:52:51 +0530

    Update: callback function

---
+++ app.js
@@ -0,0 +1,6 @@
+var btnTranslate = document.querySelector("#btn-Translate");
+var txtInput = document.querySelector("#txt-Input");
+btnTranslate.addEventListener("click", clickHandler());
+function clickHandler() {
+  console.log("Clicked!!");
+};
